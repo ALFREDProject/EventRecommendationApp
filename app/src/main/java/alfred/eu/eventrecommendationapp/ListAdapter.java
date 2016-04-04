6e042376121cf92ff6637c43bb9636ab7f34f337
juan.rodriguezp <juan.rodriguezp@atos.net>
2016-04-04 16:09:57 +0200

    Recommendations list (sim)

--- app/src/main/java/alfred/eu/eventrecommendationapp/ListAdapter.java
+++ app/src/main/java/alfred/eu/eventrecommendationapp/ListAdapter.java
@@ -13,47 +13,49 @@ import java.util.List;
  */
 public abstract class ListAdapter extends BaseAdapter {
 
-    private List<?> entries;
-    private int R_layout_IdView;
-    private Context context;
+  private List<?> entries;
+  private int R_layout_IdView;
+  private Context context;
 
-    public ListAdapter(Context context, int R_layout_IdView, List<?> entries) {
-        super();
-        this.context = context;
-        this.entries = entries;
-        this.R_layout_IdView = R_layout_IdView;
-    }
+  public ListAdapter(Context context, int R_layout_IdView, List<?> entries) {
+    super();
+    this.context = context;
+    this.entries = entries;
+    this.R_layout_IdView = R_layout_IdView;
+  }
 
-    @Override
-    public View getView(int pos, View view, ViewGroup parent) {
-        //if (view == null) {
-        LayoutInflater vi = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        view = vi.inflate(R_layout_IdView, null);
-        //view = vi.inflate(R_layout_IdView, parent, false);
-        //}
-        onEntry(entries.get(pos), view);
-        return view;
-    }
+  @Override
+  public View getView(int pos, View view, ViewGroup parent) {
+    //if (view == null) {
+    LayoutInflater vi = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    view = vi.inflate(R_layout_IdView, null);
+    //view = vi.inflate(R_layout_IdView, parent, false);
+    //}
+    onEntry(entries.get(pos), view);
+    return view;
+  }
 
-    @Override
-    public int getCount() {
-        return entries.size();
-    }
+  @Override
+  public int getCount() {
+    return entries.size();
+  }
 
-    @Override
-    public Object getItem(int posicion) {
-        return entries.get(posicion);
-    }
+  @Override
+  public Object getItem(int posicion) {
+    return entries.get(posicion);
+  }
 
-    @Override
-    public long getItemId(int posicion) {
-        return posicion;
-    }
+  @Override
+  public long getItemId(int posicion) {
+    return posicion;
+  }
 
-    /** Each entry
-     * @param entry of type handler asociated to view
-     * @param view with data from handler
-     */
-    public abstract void onEntry (Object entry, View view);
+  /**
+   * Each entry
+   *
+   * @param entry of type handler asociated to view
+   * @param view  with data from handler
+   */
+  public abstract void onEntry(Object entry, View view);
 
 }
--- app/src/main/java/alfred/eu/eventrecommendationapp/MainActivity.java
+++ app/src/main/java/alfred/eu/eventrecommendationapp/MainActivity.java
@@ -19,92 +19,97 @@ import eu.alfred.ui.CircleButton;
 
 public class MainActivity extends AppActivity {
 
-    private static final String LOGTAG = MainActivity.class.getSimpleName();
+  private static final String LOGTAG = MainActivity.class.getSimpleName();
 
-    //Action
-    private static final String GET_RECOMMENDATIONS_FOR_USER = "GetRecommendationsForUser";
+  //Action
+  private static final String GET_RECOMMENDATIONS_FOR_USER = "GetRecommendationsForUser";
 
-    private RecommendationManager recommendationManager;
-    private List<Event> recommendations;
+  private RecommendationManager recommendationManager;
+  private List<Event> recommendations;
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-        Log.d(LOGTAG, "Event recommendations app created");
+    Log.d(LOGTAG, "Event recommendations app created");
 
-        personalAssistant.setOnPersonalAssistantConnectionListener(new PersonalAssistantConnection() {
-            @Override
-            public void OnConnected() {
-                recommendationManager = new RecommendationManager(personalAssistant.getMessenger());
-                onNewIntent(getIntent());
-            }
+    personalAssistant.setOnPersonalAssistantConnectionListener(new PersonalAssistantConnection() {
+      @Override
+      public void OnConnected() {
+        recommendationManager = new RecommendationManager(personalAssistant.getMessenger());
+        onNewIntent(getIntent());
+      }
 
-            @Override
-            public void OnDisconnected() {
-                // Do some cleanup stuff
-            }
-        });
+      @Override
+      public void OnDisconnected() {
+        // Do some cleanup stuff
+      }
+    });
 
-        //Build list of alfred recommendations
+    // View contents
+    setContentView(alfred.eu.eventrecommendationapp.R.layout.activity_main);
 
-        // *********** Simulated ****************
-        recommendations = new ArrayList<>();
-        Event event = new Event();
-        event.setTitle("Recommendation 1");
-        recommendations.add(event);
-        event = new Event();
-        event.setTitle("Recommendation 2");
-        recommendations.add(event);
-        event = new Event();
-        event.setTitle("Recommendation 3");
-        recommendations.add(event);
-        displayRecommendations();
-        Log.d(LOGTAG, "Alfred simulated recommendations: " + recommendations.size());
-        // **************************************
+    // Build list of alfred recommendations
 
-        //Change your view contents. Note, the the button has to be included last.
-        setContentView(alfred.eu.eventrecommendationapp.R.layout.activity_main);
+    // *********** Simulated ****************
+    recommendations = getSimulatedEvents();
+    displayRecommendations();
+    Log.d(LOGTAG, "Alfred simulated recommendations: " + recommendations.size());
+    // **************************************
 
-        circleButton = (CircleButton) findViewById(R.id.voiceControlBtn);
-        circleButton.setOnTouchListener(new CircleTouchListener());
+    // PA Buttons
+    circleButton = (CircleButton) findViewById(R.id.voiceControlBtn);
+    circleButton.setOnTouchListener(new CircleTouchListener());
+  }
+
+  @Override
+  public void performAction(String command, Map<String, String> map) {
+    Log.d(LOGTAG, "Action performed!");
+    switch (command) {
+      case (GET_RECOMMENDATIONS_FOR_USER):
+        GetRecommendationsForUserAction cta = new GetRecommendationsForUserAction(this, cade, recommendationManager);
+        cta.performAction(command, map);
+        break;
+      default:
+        break;
     }
+  }
 
-    @Override
-    public void performAction(String command, Map<String, String> map) {
-        Log.d(LOGTAG, "Action performed!");
-        switch (command) {
-            case (GET_RECOMMENDATIONS_FOR_USER):
-                GetRecommendationsForUserAction cta = new GetRecommendationsForUserAction(this, cade,recommendationManager);
-                cta.performAction(command, map);
-                break;
-            default:
-                break;
-        }
-    }
+  public void setRecommendations(List<Event> recommendations) {
+    this.recommendations = recommendations;
+  }
 
-    public void setRecommendations(List<Event> recommendations) {
-        this.recommendations = recommendations;
-    }
+  public void displayRecommendations() {
+    ListView listViewRecommendations = (ListView) findViewById(R.id.listViewRecommendations);
+    listViewRecommendations.setAdapter(new ListAdapter(this, R.layout.every_item_recommendations_list, recommendations) {
+      @Override
+      public void onEntry(Object entry, View view) {
+        final Event event = (Event) entry;
+        TextView textViewTitle = (TextView) view.findViewById(R.id.textViewTitle);
+        TextView textViewWhen = (TextView) view.findViewById(R.id.textViewWhen);
+        TextView textViewFriendsGoing = (TextView) view.findViewById(R.id.textViewFriendsGoing);
 
-    public void displayRecommendations() {
-        ListView listViewRecommendations = (ListView) findViewById(R.id.listViewRecommendations);
-        listViewRecommendations.setAdapter(new ListAdapter(this, R.layout.every_item_recommendations_list, recommendations) {
-            @Override
-            public void onEntry(Object entry, View view) {
-                final Event event = (Event) entry;
-                TextView textViewTitle = (TextView) view.findViewById(R.id.textViewTitle);
-                TextView textViewWhen = (TextView) view.findViewById(R.id.textViewWhen);
-                TextView textViewFriendsGoing = (TextView) view.findViewById(R.id.textViewFriendsGoing);
+        //TODO fill recommendations display
+        textViewTitle.setText("TITLE: " + event.getTitle());
 
-                //TODO fill recommendations display
-                textViewTitle.setText("TITLE: " + event.getTitle());
+        //TODO setOnClickListener
 
-                //TODO setOnClickListener
+      }
+    });
+  }
 
-            }
-        });
-
-    }
+  private List<Event> getSimulatedEvents() {
+    List<Event> result = new ArrayList<>();
+    Event event = new Event();
+    event.setTitle("Recommendation 1");
+    result.add(event);
+    event = new Event();
+    event.setTitle("Recommendation 2");
+    result.add(event);
+    event = new Event();
+    event.setTitle("Recommendation 3");
+    result.add(event);
+    return result;
+  }
 }

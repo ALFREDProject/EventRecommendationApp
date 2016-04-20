bcc5a87be31c9b9d539dd644daf0717efef6e221
juan.rodriguezp <juan.rodriguezp@atos.net>
2016-04-20 17:36:14 +0200

    Add recommendation engine files
    EventRecommendationResponse and RecommendationReason

---
+++ app/src/main/java/alfred/eu/eventrecommendationapp/recommendationengine/model/RecommendationReason.java
@@ -0,0 +1,12 @@
+package alfred.eu.eventrecommendationapp.recommendationengine.model;
+
+/**
+ * Created by thardes on 20/04/2016.
+ */
+public enum RecommendationReason {
+    FRIENDS_GOING,
+    TAGS,
+    HISTORY,
+    INTERESTS,
+    SIMILAR_PARTICIPANTS, DISTANCE
+}
